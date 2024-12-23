de99b18357712dd53f138434eefacde12140548b
danny bengal <dannybengal@cdxxotus.com>
2024-12-23 10:31:10 +0100

    wss

--- src/components/Init/Init.tsx
+++ src/components/Init/Init.tsx
@@ -7,7 +7,7 @@ import {
 import useWebSocket from "../../hooks/useWebSocket";
 
 const WEBSOCKET_URL =
-  "wss://3rq6zyryb4.execute-api.us-east-1.amazonaws.com/dev";
+  "wss://hohowdv45h.execute-api.us-east-1.amazonaws.com/dev";
 
 const RECONNECT_INTERVALS = [1000, 2000, 5000, 10000]; // Exponential backoff intervals (ms)
 
