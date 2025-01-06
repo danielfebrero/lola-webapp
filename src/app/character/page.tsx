120ae53449fa9c10890942957fb18b3bd515ba1c
danny bengal <dannybengal@cdxxotus.com>
2025-01-06 15:23:38 +0100

    test

--- src/app/character/page.tsx
+++ src/app/character/page.tsx
@@ -1,5 +1,16 @@
+import LeftPanelLayout from "../../components/Layouts/LeftPanel";
+
 const CharacterPage: React.FC = () => {
-  return <>hello</>;
+  setTimeout(() => {
+    typeof window !== "undefined" &&
+      window.history.pushState(null, "", "/character/new");
+  }, 1000);
+
+  return (
+    <>
+      <LeftPanelLayout />
+    </>
+  );
 };
 
 export default CharacterPage;
