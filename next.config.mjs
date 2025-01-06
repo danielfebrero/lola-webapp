d594f992f399e2e44ac6bd1cec8c5894bf1be24c
danny bengal <dannybengal@cdxxotus.com>
2025-01-06 15:06:11 +0100

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // Outputs a Single-Page Application (SPA).
-  distDir: "./build", // Changes the build output directory to `./build`.
-};
+const nextConfig = {};
 
 export default nextConfig;
