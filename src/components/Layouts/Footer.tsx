03b7c991a233e54c08a0d21e930e9c1350dfb7fc
danny bengal <dannybengal@cdxxotus.com>
2025-01-06 19:48:25 +0100

    test

---
+++ src/components/Layouts/Footer.tsx
@@ -0,0 +1,5 @@
+const Footer: React.FC = () => {
+  return <div className="w-full text-center"></div>;
+};
+
+export default Footer;
--- src/components/Layouts/Page.tsx
+++ src/components/Layouts/Page.tsx
@@ -1,6 +1,6 @@
 import LeftPanelLayout from "./LeftPanel";
 import HeaderLayout from "./Header";
-import Footer from "../Footer";
+import Footer from "./Footer";
 
 import "../../index.css";
 
