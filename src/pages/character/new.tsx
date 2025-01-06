57fc8f7a628ecc806d58a04a2f7afc57df9f93b9
danny bengal <dannybengal@cdxxotus.com>
2025-01-06 19:30:26 +0100

    test

--- src/pages/character/new.tsx
+++ src/pages/character/new.tsx
@@ -12,9 +12,9 @@ const NewCharacterPage: React.FC = () => {
   return (
     <div className="no-scrollbar overflow-hidden h-screen w-screen">
       <PageLayout>test</PageLayout>
-      {/* <div className="fixed w-screen h-screen top-0 left-0 z-1">
+      <div className="fixed w-screen h-screen top-0 left-0 z-1">
         <App />
-      </div> */}
+      </div>
     </div>
   );
 };
