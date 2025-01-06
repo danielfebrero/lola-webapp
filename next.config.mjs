6dc434515d288cac84d72bdf17c29ea1b124479a
danny bengal <dannybengal@cdxxotus.com>
2025-01-06 14:08:59 +0100

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: "./build", // Changes the build output directory to `./build`.
+  reactStrictMode: true,
 };
 
 export default nextConfig;
