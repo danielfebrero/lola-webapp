0487e79ae2b0690b1acd62077a0df7c809678811
danny bengal <dannybengal@cdxxotus.com>
2025-01-07 21:03:01 +0100

    x

--- src/pages/login/[...slug]/index.tsx
+++ src/pages/login/[...slug]/index.tsx
@@ -10,9 +10,7 @@ const App = dynamic(() => import("../../../App"), {
 const CharacterPage: React.FC = () => {
   return (
     <div className="no-scrollbar overflow-hidden h-screen w-screen">
-      <PageLayout headerDropdownLabel={"Character"}>
-        <CharacterLayout />
-      </PageLayout>
+      <PageLayout headerDropdownLabel={"Character"}>{""}</PageLayout>
       <div className="fixed w-screen h-screen top-0 left-0 z-1">
         <App />
       </div>
