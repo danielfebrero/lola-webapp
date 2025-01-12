a650f25ae3de71364e3d37f3356049f7cfd44450
danny bengal <dannybengal@cdxxotus.com>
2025-01-12 15:17:10 +0100

    pwa ios

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,10 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#3ccccf" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+          <link rel="apple-touch-icon" href="/logo192.png" />
+          <link rel="apple-touch-icon" sizes="512x512" href="/logo512.png" />
           <title>Lola</title>
           <meta
             name="description"
