ecf1840e57b12b795dedd1abf7f439ff0e248971
danny bengal <dannybengal@cdxxotus.com>
2025-01-16 16:05:10 +0100

    lola18 in profile dropdown

--- src/components/ProfileDropdown/ProfileDropdown.tsx
+++ src/components/ProfileDropdown/ProfileDropdown.tsx
@@ -4,9 +4,10 @@ import { useTranslation } from "react-i18next";
 import useClickOutside from "../../hooks/useClickOutside";
 import LogoutIcon from "../../icons/logout";
 import SettingsIcon from "../../icons/setting";
+import AdultIcon from "../../icons/adult";
 import useGA from "../../hooks/useGA";
-import { useAppDispatch } from "../../store/hooks";
-import { toggleSettings } from "../../store/features/app/appSlice";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { toggleSettings, setMode } from "../../store/features/app/appSlice";
 
 interface ProfileDropdownProps {
   hide: () => void;
@@ -18,6 +19,7 @@ const ProfileDropdown: React.FC<ProfileDropdownProps> = (props) => {
   const ref = useClickOutside(() => {
     props.hide();
   });
+  const { mode } = useAppSelector((state) => state.app);
   const { sendEvent } = useGA();
   const dispatch = useAppDispatch();
 
@@ -38,10 +40,34 @@ const ProfileDropdown: React.FC<ProfileDropdownProps> = (props) => {
       ref={ref}
       className="rounded-lg border dark:border-darkBorderLight border-borderLight shadow pl-[5px] pr-[5px] pt-[5px] pb-[5px] w-fit absolute z-10 bg-white dark:bg-darkMainSurfaceSecondary min-w-[260px] right-[20px] mt-[40px]"
     >
+      <div
+        className="cursor-pointer hover:bg-lightGray dark:hover:bg-darkMainSurcaceTertiary p-[10px] flex flex-row items-center dark:text-pink-200 text-rose-600"
+        onClick={() => {
+          dispatch(setMode(mode === "adult" ? "minor" : "adult"));
+          props.hide();
+          sendEvent(
+            mode === "adult"
+              ? "click_exit_lola18_from_profile_dropdown"
+              : "click_enter_lola18_from_profile_dropdown"
+          );
+        }}
+      >
+        <div className="h-[20px] w-[20px]">
+          <AdultIcon />
+        </div>
+        <div className="ml-[10px]">
+          {t(
+            mode === "minor" ? "Enter Lola for Adults" : "Exit Lola for Adults"
+          )}
+        </div>
+      </div>
       {auth?.isAuthenticated ? (
         <div
           className="rounded-lg cursor-pointer hover:bg-lightGray dark:hover:bg-darkMainSurcaceTertiary p-[10px] flex flex-row items-center"
-          onClick={signOutRedirect}
+          onClick={() => {
+            signOutRedirect();
+            sendEvent("click_signout");
+          }}
         >
           <div className="h-[20px] w-[20px] text-textSecondary dark:text-darkTextSecondary">
             <LogoutIcon />
@@ -64,7 +90,10 @@ const ProfileDropdown: React.FC<ProfileDropdownProps> = (props) => {
       )}
       <div
         className="cursor-pointer hover:bg-lightGray dark:hover:bg-darkMainSurcaceTertiary p-[10px] flex flex-row items-center"
-        onClick={() => dispatch(toggleSettings())}
+        onClick={() => {
+          dispatch(toggleSettings());
+          props.hide();
+        }}
       >
         <div className="h-[20px] w-[20px] text-textSecondary dark:text-darkTextSecondary">
           <SettingsIcon />
