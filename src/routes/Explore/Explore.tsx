27e31dde90ba05ed5f2e3e4d948f5425f62c9a5b
danny bengal <dannybengal@cdxxotus.com>
2025-01-19 23:01:43 +0100

    ok

--- src/routes/Explore/Explore.tsx
+++ src/routes/Explore/Explore.tsx
@@ -34,7 +34,9 @@ const ExplorePage: React.FC<ExplorePageProps> = (props) => {
     socketConnection,
   } = useWebSocket({});
   const [searchParams] = useSearchParams();
-  const { explore, exploreLanguage } = useAppSelector((state) => state.app);
+  const { explore, exploreLanguage, isLeftPanelOpen } = useAppSelector(
+    (state) => state.app
+  );
   const { clickedUpvotes, clickedDownvotes } = useAppSelector(
     (state) => state.user
   );
@@ -73,21 +75,24 @@ const ExplorePage: React.FC<ExplorePageProps> = (props) => {
               (c) => (
                 <div
                   className={clsx(
-                    "p-[10px] hover:bg-lightGray rounded-lg dark:hover:bg-darkMainSurfaceSecondary border-b border-borderColor dark:border-darkBorderColor"
+                    "p-[10px] hover:bg-lightGray rounded-lg dark:hover:bg-darkMainSurfaceSecondary border-b border-borderColor dark:border-darkBorderColor w-full items-center flex flex-col"
                   )}
                 >
-                  <div className="flex flex-col h-auto overflow-y-hidden cursor-pointer">
-                    <Link to={"/" + c.thread.type + "/" + c.thread.threadId}>
-                      <div className="font-bold mb-[10px] text-lg">
-                        {c.thread.title}
-                      </div>
-                    </Link>
-                    ,
-                    <div
-                      className={clsx({
-                        "max-w-[715px]": c.thread.type === "story",
-                      })}
-                    >
+                  <div
+                    className={clsx(
+                      { "max-w-[715px]": c.thread.type === "story" },
+                      "flex flex-col h-auto overflow-y-hidden cursor-pointer w-full"
+                    )}
+                  >
+                    {c.thread.type === "story" && (
+                      <Link to={"/" + c.thread.type + "/" + c.thread.threadId}>
+                        <div className="font-bold mb-[10px] text-lg self-middle">
+                          {c.thread.title}
+                        </div>
+                      </Link>
+                    )}
+
+                    <div>
                       {c.thread.type === "story" &&
                         c.story?.image_search_results &&
                         c.story.image_search_results.length > 0 && (
@@ -120,117 +125,155 @@ const ExplorePage: React.FC<ExplorePageProps> = (props) => {
                             )
                           )}
                         {c.thread.type === "character" && (
-                          <div className="flex flex-row">
-                            <div
-                              className={clsx(
-                                "h-[120px] w-[120px] rounded-full bg-slate-200 items-center flex flex-shrink-0"
-                              )}
-                            >
-                              {c.character?.imagesMultisize &&
-                              c.character?.imagesMultisize.length > 0 ? (
-                                <img
-                                  alt={c.character?.json?.name}
-                                  className={clsx("rounded-full object-cover")}
-                                  src={c.character?.imagesMultisize[0].large}
-                                />
-                              ) : null}
-                            </div>
-                            <div className="flex-shrink-0 mr-[20px] md:max-w-[200px]">
-                              <JSONToText data={c.character?.json ?? {}} />
-                            </div>
-                            <div className="hidden md:block">
-                              {c.thread.chatLog?.slice(-2).map((message, idx) =>
-                                message.role === "user" ? (
-                                  <div
-                                    className="flex flex-row justify-end mb-[10px]"
-                                    key={message.id}
-                                  >
-                                    <div
+                          <>
+                            <div className="flex flex-row">
+                              <div
+                                className={clsx(
+                                  {
+                                    "w-[calc(50vw-550px)]": isLeftPanelOpen,
+                                    "w-[calc(50vw-420px)]": !isLeftPanelOpen,
+                                  },
+                                  "min-w-[120px] md:mr-0 mr-[20px] transition-all duration-500 justify-center flex"
+                                )}
+                              >
+                                <div
+                                  className={clsx(
+                                    "h-[120px] w-[120px] rounded-full bg-slate-200 items-center flex flex-shrink-0"
+                                  )}
+                                >
+                                  {c.character?.imagesMultisize &&
+                                  c.character?.imagesMultisize.length > 0 ? (
+                                    <img
+                                      alt={c.character?.json?.name}
                                       className={clsx(
-                                        "bg-messageBackground dark:bg-darkMessageBackground rounded-lg p-[10px]"
+                                        "rounded-full object-cover"
                                       )}
-                                    >
-                                      <Markdown>{message.content}</Markdown>
-                                    </div>
-                                  </div>
-                                ) : (
-                                  <div className="" key={message.id}>
-                                    <Markdown>{message.content}</Markdown>
-                                  </div>
-                                )
-                              )}
+                                      src={
+                                        c.character?.imagesMultisize[0].large
+                                      }
+                                    />
+                                  ) : null}
+                                </div>
+                              </div>
+                              <div className="max-w-[715px] flex flex-row">
+                                <div className="flex-shrink-0 mr-[20px]">
+                                  <JSONToText data={c.character?.json ?? {}} />
+                                </div>
+                                <div className="hidden md:block max-w-[715px]">
+                                  {c.thread.chatLog
+                                    ?.slice(-2)
+                                    .map((message, idx) =>
+                                      message.role === "user" ? (
+                                        <div
+                                          className="flex flex-row justify-end mb-[10px]"
+                                          key={message.id}
+                                        >
+                                          <div
+                                            className={clsx(
+                                              "bg-messageBackground dark:bg-darkMessageBackground rounded-lg p-[10px]"
+                                            )}
+                                          >
+                                            <Markdown>
+                                              {message.content}
+                                            </Markdown>
+                                          </div>
+                                        </div>
+                                      ) : (
+                                        <div className="" key={message.id}>
+                                          <Markdown>{message.content}</Markdown>
+                                        </div>
+                                      )
+                                    )}
+                                </div>
+                              </div>
                             </div>
-                          </div>
+                          </>
                         )}
                       </Link>
                     </div>
                   </div>
-                  <div className="flex flex-row mt-[10px]">
-                    <div className="flex flex-row rounded-lg dark:bg-darkMainSurcaceTertiary bg-gray-200 items-center">
-                      <div
-                        onClick={() => {
-                          if (
-                            !stateClickedUpvotes.includes(c.thread.threadId)
-                          ) {
-                            upvote(c.thread.threadId);
-                            sendEvent("upvote", "explore");
-                            dispatch(upvoteExplore(c.thread.threadId));
-                            setStateClickedUpvotes((prev) => [
-                              ...prev,
-                              c.thread.threadId,
-                            ]);
-                            if (
-                              stateClickedDownvotes.includes(c.thread.threadId)
-                            ) {
-                              dispatch(upvoteExplore(c.thread.threadId));
-                              setStateClickedDownvotes((prev) =>
-                                prev.filter((t) => t !== c.thread.threadId)
-                              );
-                            }
-                          }
-                        }}
-                        className={clsx(
-                          {
-                            "dark:bg-darkMainSurfacePrimary bg-white":
-                              stateClickedUpvotes.includes(c.thread.threadId),
-                          },
-                          "cursor-pointer w-[30px] h-[30px] hover:dark:bg-darkMainSurfacePrimary hover:bg-white p-[5px] rounded-full"
-                        )}
-                      >
-                        <UpvoteIcon />
-                      </div>
-                      <span className="mx-[5px]">{c.thread.votes ?? 0}</span>
-                      <div
-                        onClick={() => {
-                          if (
-                            !stateClickedDownvotes.includes(c.thread.threadId)
-                          ) {
-                            downvote(c.thread.threadId);
-                            sendEvent("downvote", "explore");
-                            dispatch(downvoteExplore(c.thread.threadId));
-                            setStateClickedDownvotes((prev) => [
-                              ...prev,
-                              c.thread.threadId,
-                            ]);
-                            if (
-                              stateClickedUpvotes.includes(c.thread.threadId)
-                            ) {
-                              dispatch(downvoteExplore(c.thread.threadId));
-                              setStateClickedUpvotes((prev) =>
-                                prev.filter((t) => t !== c.thread.threadId)
-                              );
-                            }
-                          }
-                        }}
-                        className={clsx(
-                          {
-                            "dark:bg-darkMainSurfacePrimary bg-white":
-                              stateClickedDownvotes.includes(c.thread.threadId),
-                          },
-                          "cursor-pointer w-[30px] h-[30px] hover:dark:bg-darkMainSurfacePrimary hover:bg-white p-[5px] rounded-full"
-                        )}
-                      >
-                        <DownvoteIcon />
+                  <div className="flex flex-row w-full justify-center">
+                    <div className="flex flex-row w-full max-w-[715px]">
+                      <div className="flex flex-row mt-[10px]">
+                        <div className="flex flex-row rounded-lg dark:bg-darkMainSurcaceTertiary bg-gray-200 items-center">
+                          <div
+                            onClick={() => {
+                              if (
+                                !stateClickedUpvotes.includes(c.thread.threadId)
+                              ) {
+                                upvote(c.thread.threadId);
+                                sendEvent("upvote", "explore");
+                                dispatch(upvoteExplore(c.thread.threadId));
+                                setStateClickedUpvotes((prev) => [
+                                  ...prev,
+                                  c.thread.threadId,
+                                ]);
+                                if (
+                                  stateClickedDownvotes.includes(
+                                    c.thread.threadId
+                                  )
+                                ) {
+                                  dispatch(upvoteExplore(c.thread.threadId));
+                                  setStateClickedDownvotes((prev) =>
+                                    prev.filter((t) => t !== c.thread.threadId)
+                                  );
+                                }
+                              }
+                            }}
+                            className={clsx(
+                              {
+                                "dark:bg-darkMainSurfacePrimary bg-white":
+                                  stateClickedUpvotes.includes(
+                                    c.thread.threadId
+                                  ),
+                              },
+                              "cursor-pointer w-[30px] h-[30px] hover:dark:bg-darkMainSurfacePrimary hover:bg-white p-[5px] rounded-full"
+                            )}
+                          >
+                            <UpvoteIcon />
+                          </div>
+                          <span className="mx-[5px]">
+                            {c.thread.votes ?? 0}
+                          </span>
+                          <div
+                            onClick={() => {
+                              if (
+                                !stateClickedDownvotes.includes(
+                                  c.thread.threadId
+                                )
+                              ) {
+                                downvote(c.thread.threadId);
+                                sendEvent("downvote", "explore");
+                                dispatch(downvoteExplore(c.thread.threadId));
+                                setStateClickedDownvotes((prev) => [
+                                  ...prev,
+                                  c.thread.threadId,
+                                ]);
+                                if (
+                                  stateClickedUpvotes.includes(
+                                    c.thread.threadId
+                                  )
+                                ) {
+                                  dispatch(downvoteExplore(c.thread.threadId));
+                                  setStateClickedUpvotes((prev) =>
+                                    prev.filter((t) => t !== c.thread.threadId)
+                                  );
+                                }
+                              }
+                            }}
+                            className={clsx(
+                              {
+                                "dark:bg-darkMainSurfacePrimary bg-white":
+                                  stateClickedDownvotes.includes(
+                                    c.thread.threadId
+                                  ),
+                              },
+                              "cursor-pointer w-[30px] h-[30px] hover:dark:bg-darkMainSurfacePrimary hover:bg-white p-[5px] rounded-full"
+                            )}
+                          >
+                            <DownvoteIcon />
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
