ab7e22da35cc4bed82776fb717ca2ddabc7d9c9f
danny bengal <dannybengal@cdxxotus.com>
2025-02-04 16:59:21 +0100

    gné

--- src/App.tsx
+++ src/App.tsx
@@ -65,61 +65,72 @@ const App: React.FC = () => {
             api_key={"JwhVr4bpczMbGntosgQkjduKjQU37hYV"}
             project={"Lola.la"}
           >
-            <div className="dark:bg-darkMainSurfacePrimary text-textPrimary dark:text-darkTextPrimary flex h-screen w-screen items-center justify-center">
+            {/* <div className="dark:bg-darkMainSurfacePrimary text-textPrimary dark:text-darkTextPrimary flex h-screen w-screen items-center justify-center">
               We are offline for maintenance. Please check back tomorrow.
-            </div>
-            {/* <BrowserRouter>
-              <Analytics />
-              <SpeedInsights />
-              <Init />
-              <Routes>
-                <Route element={<MainLayout />}>
-                  <Route
-                    path="/"
-                    element={<Navigate to="/explore/latest" replace={true} />}
-                  />
-                  <Route
-                    path="/18"
-                    element={
-                      <Navigate to="/explore/latest?adult=1" replace={true} />
-                    }
-                  />
-                  <Route path="/login/silent-renew" element={<SilentRenew />} />
-                  <Route path="/login/success" element={<LoginSuccess />} />
-                  <Route
-                    path="/explore/images"
-                    element={<ExploreImagesPage />}
-                  />
-                  <Route path="/explore/:type" element={<ExplorePage />} />
-                  <Route
-                    path="/character/main"
-                    element={<CharacterPage selected={{ type: "main" }} />}
-                  />
-                  <Route
-                    path="/character/:threadId"
-                    element={<CharacterPage />}
-                  />
-                  <Route path="/game" element={<GamePage />} />
-                  <Route path="/game/new" element={<NewGamePage />} />
-                  <Route path="/game/:threadId" element={<GamePage />} />
-                  <Route path="/story/:threadId" element={<StoryPage />} />
-                  <Route path="/story/new" element={<NewStoryPage />} />
-                  <Route path="/lola/:threadId" element={<LolaPage />} />
-                  <Route path="/analytics/:type" element={<AnalyticsPage />} />
-                  <Route path="/pricing" element={<PricingPage />} />
-                  <Route
-                    path="/checkout/order-received/:orderId"
-                    element={<OrderReceivedPage />}
-                  />
-                  <Route
-                    path="/checkout/order-canceled/:orderId"
-                    element={<OrderCanceledPage />}
-                  />
-                </Route>
+            </div> */}
+            {
+              <BrowserRouter>
+                <Analytics />
+                <SpeedInsights />
+                <Init />
+                <Routes>
+                  <Route element={<MainLayout />}>
+                    <Route
+                      path="/"
+                      element={<Navigate to="/explore/latest" replace={true} />}
+                    />
+                    <Route
+                      path="/18"
+                      element={
+                        <Navigate to="/explore/latest?adult=1" replace={true} />
+                      }
+                    />
+                    <Route
+                      path="/login/silent-renew"
+                      element={<SilentRenew />}
+                    />
+                    <Route path="/login/success" element={<LoginSuccess />} />
+                    <Route
+                      path="/explore/images"
+                      element={<ExploreImagesPage />}
+                    />
+                    <Route path="/explore/:type" element={<ExplorePage />} />
+                    <Route
+                      path="/character/main"
+                      element={<CharacterPage selected={{ type: "main" }} />}
+                    />
+                    <Route
+                      path="/character/:threadId"
+                      element={<CharacterPage />}
+                    />
+                    <Route path="/game" element={<GamePage />} />
+                    <Route path="/game/new" element={<NewGamePage />} />
+                    <Route path="/game/:threadId" element={<GamePage />} />
+                    <Route path="/story/:threadId" element={<StoryPage />} />
+                    <Route path="/story/new" element={<NewStoryPage />} />
+                    <Route path="/lola/:threadId" element={<LolaPage />} />
+                    <Route
+                      path="/analytics/:type"
+                      element={<AnalyticsPage />}
+                    />
+                    <Route path="/pricing" element={<PricingPage />} />
+                    <Route
+                      path="/checkout/order-received/:orderId"
+                      element={<OrderReceivedPage />}
+                    />
+                    <Route
+                      path="/checkout/order-canceled/:orderId"
+                      element={<OrderCanceledPage />}
+                    />
+                  </Route>
 
-                <Route path="*" element={<Navigate to="/" replace={true} />} />
-              </Routes>
-            </BrowserRouter> */}
+                  <Route
+                    path="*"
+                    element={<Navigate to="/" replace={true} />}
+                  />
+                </Routes>
+              </BrowserRouter>
+            }
           </UserLogProvider>
         </PersistGate>
       </Provider>
