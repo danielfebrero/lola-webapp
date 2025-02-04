cf3edae8b66ff999243a5d39b0a2f2a0e02a9329
danny bengal <dannybengal@cdxxotus.com>
2025-02-04 16:37:11 +0100

    prepare for prod

--- src/components/Init/Init.tsx
+++ src/components/Init/Init.tsx
@@ -22,7 +22,8 @@ import { setScenarios } from "../../store/features/games/gamesSlice";
 
 const WEBSOCKET_URL =
   process.env.NODE_ENV === "development" ||
-  window.location.origin !== "https://lola.la"
+  (window.location.origin !== "https://lola.la" &&
+    window.location.origin !== "https://fabularius.ai")
     ? "wss://6nk800sp9d.execute-api.us-east-1.amazonaws.com/dev"
     : "wss://ktufwkytp1.execute-api.us-east-1.amazonaws.com/prod";
 
