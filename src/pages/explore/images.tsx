5e7450c6dbbda8cb2e75a2faa2a1784263b4df1d
danny bengal <dannybengal@cdxxotus.com>
2025-02-05 15:25:15 +0100

    add explore titles

--- src/pages/explore/images.tsx
+++ src/pages/explore/images.tsx
@@ -1,6 +1,7 @@
 import dynamic from "next/dynamic";
 
 import PageLayout from "../../components/Layouts/Page";
+import Head from "next/head";
 
 const App = dynamic(() => import("../../App"), {
   ssr: false,
@@ -9,6 +10,12 @@ const App = dynamic(() => import("../../App"), {
 const ExplorePage: React.FC = () => {
   return (
     <div className="no-scrollbar overflow-hidden h-screen w-screen">
+      <Head>
+        <title>
+          Explore images on Fabularius AI - Choose your own adventure,
+          storyteller, chatbot, character and image generator.
+        </title>
+      </Head>
       <PageLayout headerDropdownLabel="Images">{""}</PageLayout>
       <div className="fixed w-screen h-screen top-0 left-0 z-1">
         <App />
