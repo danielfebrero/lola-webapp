36879e903f68ad89c4046ff2baeed58c05924c3f
danny bengal <dannybengal@cdxxotus.com>
2025-02-05 15:29:26 +0100

    new lola ssr

---
+++ src/pages/lola/new.tsx
@@ -0,0 +1,27 @@
+import dynamic from "next/dynamic";
+
+import PageLayout from "../../components/Layouts/Page";
+import Head from "next/head";
+
+const App = dynamic(() => import("../../App"), {
+  ssr: false,
+});
+
+const NewLolaPage: React.FC = () => {
+  return (
+    <div className="no-scrollbar overflow-hidden h-screen w-screen">
+      <Head>
+        <title>
+          Lola chatbot on Fabularius AI - Choose your own adventure,
+          storyteller, chatbot, character and image generator.
+        </title>
+      </Head>
+      <PageLayout headerDropdownLabel="Lola">{""}</PageLayout>
+      <div className="fixed w-screen h-screen top-0 left-0 z-1">
+        <App />
+      </div>
+    </div>
+  );
+};
+
+export default NewLolaPage;
