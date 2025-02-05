10649064b568fa9c219cf6aaa931464dd372ea65
danny bengal <dannybengal@cdxxotus.com>
2025-02-05 15:27:11 +0100

    'landing' page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import { Head } from "next/document";
 import dynamic from "next/dynamic";
+import PageLayout from "../components/Layouts/Page";
 
 const App = dynamic(() => import("../App"), {
   ssr: false,
@@ -7,6 +9,13 @@ const App = dynamic(() => import("../App"), {
 const CharacterPage: React.FC = () => {
   return (
     <div className="no-scrollbar overflow-hidden h-screen w-screen">
+      <Head>
+        <title>
+          Fabularius AI - Choose your own adventure, storyteller, chatbot,
+          character and image generator.
+        </title>
+      </Head>
+      <PageLayout>{""}</PageLayout>
       <div className="fixed w-screen h-screen top-0 left-0 z-1">
         <App />
       </div>
