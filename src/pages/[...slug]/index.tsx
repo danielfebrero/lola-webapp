c998946f816308d298e4eadb59e5cae35a463621
danny bengal <dannybengal@cdxxotus.com>
2025-02-06 21:35:07 +0100

    ok

--- src/pages/[...slug]/index.tsx
+++ src/pages/[...slug]/index.tsx
@@ -1,15 +1,33 @@
 import dynamic from "next/dynamic";
+import Head from "next/head";
 
 import PageLayout from "../../components/Layouts/Page";
+import ExploreFeedLayout from "../../components/Layouts/ExploreFeed";
+import { GetServerSideProps } from "next";
+import { Character } from "../../types/characters";
+import { getAPIUrlFromContext } from "../../utils/ssr";
 
 const App = dynamic(() => import("../../App"), {
   ssr: false,
 });
 
-const LandingPage: React.FC = () => {
+interface ExploreLatestPageProps {
+  data: {
+    thread: ChatLog;
+    character: Character;
+    story: Story;
+  }[];
+}
+
+const ExploreLatestPage: React.FC<ExploreLatestPageProps> = ({ data }) => {
   return (
     <div className="no-scrollbar overflow-hidden h-screen w-screen">
-      <PageLayout headerDropdownLabel="Latest">{""}</PageLayout>
+      <Head>
+        <title>Explore latest content on Fabularius AI</title>
+      </Head>
+      <PageLayout headerDropdownLabel="Latest">
+        <ExploreFeedLayout data={data} />
+      </PageLayout>
       <div className="fixed w-screen h-screen top-0 left-0 z-1">
         <App />
       </div>
@@ -17,4 +35,26 @@ const LandingPage: React.FC = () => {
   );
 };
 
-export default LandingPage;
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { query } = context;
+  const adult = query.adult;
+  const fetchQuery = adult === "1" ? "?mode=adult" : "?mode=minor";
+
+  const res = await fetch(
+    getAPIUrlFromContext(context) + "/explore/latest" + fetchQuery
+  );
+
+  if (!res.ok) {
+    return { props: { data: [] } };
+  }
+
+  const result = await res.json();
+
+  return {
+    props: {
+      data: result,
+    },
+  };
+};
+
+export default ExploreLatestPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,33 @@
-import Head from "next/head";
 import dynamic from "next/dynamic";
+import Head from "next/head";
 
 import PageLayout from "../components/Layouts/Page";
+import ExploreFeedLayout from "../components/Layouts/ExploreFeed";
+import { GetServerSideProps } from "next";
+import { Character } from "../types/characters";
+import { getAPIUrlFromContext } from "../utils/ssr";
 
 const App = dynamic(() => import("../App"), {
   ssr: false,
 });
 
-const CharacterPage: React.FC = () => {
+interface ExploreLatestPageProps {
+  data: {
+    thread: ChatLog;
+    character: Character;
+    story: Story;
+  }[];
+}
+
+const ExploreLatestPage: React.FC<ExploreLatestPageProps> = ({ data }) => {
   return (
     <div className="no-scrollbar overflow-hidden h-screen w-screen">
       <Head>
-        <title>
-          Fabularius AI - Choose your own adventure, storyteller, chatbot,
-          character and image generator.
-        </title>
+        <title>Explore latest content on Fabularius AI</title>
       </Head>
-      <PageLayout>{""}</PageLayout>
+      <PageLayout headerDropdownLabel="Latest">
+        <ExploreFeedLayout data={data} />
+      </PageLayout>
       <div className="fixed w-screen h-screen top-0 left-0 z-1">
         <App />
       </div>
@@ -24,4 +35,26 @@ const CharacterPage: React.FC = () => {
   );
 };
 
-export default CharacterPage;
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { query } = context;
+  const adult = query.adult;
+  const fetchQuery = adult === "1" ? "?mode=adult" : "?mode=minor";
+
+  const res = await fetch(
+    getAPIUrlFromContext(context) + "/explore/latest" + fetchQuery
+  );
+
+  if (!res.ok) {
+    return { props: { data: [] } };
+  }
+
+  const result = await res.json();
+
+  return {
+    props: {
+      data: result,
+    },
+  };
+};
+
+export default ExploreLatestPage;
