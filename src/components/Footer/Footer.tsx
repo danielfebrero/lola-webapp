74a7ee597dd3ec16b942c696444fb9705eebec54
danny bengal <dannybengal@cdxxotus.com>
2025-02-09 21:34:21 +0100

    scroll

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 const Footer: React.FC = () => {
   const { t } = useTranslation();
   return (
-    <div className="w-full text-center whitespace-nowrap overflow-y-hidden no-scrollbar">
+    <div className="w-full text-center whitespace-nowrap overflow-y-scroll no-scrollbar">
       {t("Fabularius is a storyteller. Everything is fictional.")}
     </div>
   );
